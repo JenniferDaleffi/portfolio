b93aa5a2a8e95b00f76ea885cbcd024d43bb14f5
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-12-06 20:34:04 -0300

    update icons

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import JenniFoto from "../../public/jennifer.jpg"
 import Nav from "../components/Nav/nav"
 import Sobre from "../components/Sobre/sobre"
-
+import Github from "../../public/github.png"
 
 export default function Home() {
   return (
@@ -16,8 +16,9 @@ export default function Home() {
             <p className="mb-5">Estudante de Análise e Desenvolvimento de Sistemas com foco em desenvolvimento back-end Java e Infra & DevOps</p>
             <button className="border-2 border-solid border-bege bg-bege w-32 p-2 rounded-md text-cinza hover:bg-transparent hover:text-bege transition duration-500">Contate-me</button>
           </div>
-          <div>
-            <Image className="bg-bege rounded-full w-96 h-96" src={JenniFoto} alt="minha foto"></Image>
+          <div className="flex flex-col items-center">
+            <Image className="bg-bege rounded-full w-96 h-96 mb-5" src={JenniFoto} alt="minha foto"></Image>
+            <a href="https://github.com/JenniferDaleffi"><Image src={Github} alt="icone do github"></Image></a>
           </div>
         </div>
       </div>
