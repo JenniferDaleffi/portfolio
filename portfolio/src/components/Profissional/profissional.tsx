d079210b6453e3355d9f20b3daa207bf2ed34013
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-12-06 21:35:52 -0300

    update professional

--- portfolio/src/components/Profissional/profissional.tsx
+++ portfolio/src/components/Profissional/profissional.tsx
@@ -1,23 +1,20 @@
 export default function Profissional() {
     return (
-        <div className="bg-bege h-screen">
-            <div>
-                <h1 className="ml-10 p-20 text-3xl text-center">Meu profissional</h1>
-            </div>
-            <div className="flex justify-around mt-20">
-                <div className="border-solid border-2 border-cinza w-72 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
+        <div className="bg-bege mb-10">
+            <div className="flex gap-20 justify-around">
+                <div className="border-solid border-2 border-cinza w-96 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
                     <h1 className="text-2xl">Currículo</h1>
-                    <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos quia obcaecati cumque perferendis numquam ullam ad, totam perspiciatis recusandae atque?</p>
+                    <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequuntur atque aliquam voluptas repellendus quos nisi aut, delectus ut rerum sint, placeat commodi quasi pariatur expedita minima a. Debitis corrupti neque, praesentium vel quam odit totam earum, dolorem ducimus veniam asperiores ex molestias quia ipsum maxime non ut consequatur sit.</p>
                     <button className="border-2 border-solid border-cinza bg-cinza p-5 rounded-lg w-40 text-white hover:bg-transparent hover:text-cinza transition duration-500">Baixar CV</button>
                 </div>
-                <div className="border-solid border-2 border-cinza w-72 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
+                <div className="border-solid border-2 border-cinza w-96 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
                     <h1 className="text-2xl">Certificados</h1>
-                    <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos quia obcaecati cumque perferendis numquam ullam ad, totam perspiciatis recusandae atque?</p>
+                    <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequuntur atque aliquam voluptas repellendus quos nisi aut, delectus ut rerum sint, placeat commodi quasi pariatur expedita minima a. Debitis corrupti neque, praesentium vel quam odit totam earum, dolorem ducimus veniam asperiores ex molestias quia ipsum maxime non ut consequatur sit.</p>
                     <button className="border-2 border-solid border-cinza bg-cinza p-5 rounded-lg w-40 text-white hover:bg-transparent hover:text-cinza transition duration-500">Certificados</button>
                 </div>
-                <div className="border-solid border-2 border-cinza w-72 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
+                <div className="border-solid border-2 border-cinza w-96 h-96 p-5 rounded-xl bg-transparent shadow-slate-700 shadow-lg flex flex-col justify-between items-center hover:scale-110 hover:duration-300">
                     <h1 className="text-2xl">Experiência</h1>
-                    <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos quia obcaecati cumque perferendis numquam ullam ad, totam perspiciatis recusandae atque?</p>
+                    <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequuntur atque aliquam voluptas repellendus quos nisi aut, delectus ut rerum sint, placeat commodi quasi pariatur expedita minima a. Debitis corrupti neque, praesentium vel quam odit totam earum, dolorem ducimus veniam asperiores ex molestias quia ipsum maxime non ut consequatur sit.</p>
                     <button className="border-2 border-solid border-cinza bg-cinza p-5 rounded-lg w-40 text-white hover:bg-transparent hover:text-cinza transition duration-500">Rede Líderes</button>
                 </div>
             </div>
