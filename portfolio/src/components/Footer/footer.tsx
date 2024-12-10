82d756bcce4146262f1bf1a2231809a7ada062c3
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-12-09 23:14:01 -0300

    feat footer update

--- portfolio/src/components/Footer/footer.tsx
+++ portfolio/src/components/Footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <div>
-            <p className="text-center mb-5 mt-10">© 2024 Jennifer. Todos os direitos reservados.</p>
+            <p className="text-center mb-5 mt-10">© 2024 Jennifer Daleffi. Todos os direitos reservados.</p>
         </div>
     )
 }
