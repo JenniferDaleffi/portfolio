648a270b5a8a8324b2b7c17831d4f1849a5b9fc5
JenniferDaleffi <jenniferdaleffii@gmail.com>
2025-02-17 22:25:05 -0300

    b3

--- portfolio/src/app/experiencias/page.tsx
+++ portfolio/src/app/experiencias/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import Palestra from "../../../public/palestra.jpg"
+import Palestra from "../../../public/B3-Fachada.jpg"
 import { FaArrowLeft  } from "react-icons/fa";
 import Link from "next/link";
 
@@ -14,10 +14,10 @@ export default function Experiencia(){
                 <div className="w-3/4 mt-10 md:w-2/4 xl:w-1/4 border border-cinza rounded-lg hover:scale-110 hover:duration-300">
                     <Image className="w-full h-48 object-cover" src={Palestra} alt="aa"></Image>
                     <div className="p-5">
-                        <h1 className="mt-5 text-center text-xl font-bold">Rede Líderes Digitais</h1>
-                        <p className="text-center">Abril 2024 - Agosto 2024</p>
-                        <p className="text-center mb-5">Função: Membro</p>
-                        <p className="">A Rede Líderes Digitais é uma comunidade exclusiva que reúne as principais empresas nativas digitais, como PayPal, Mercado Livre, Shein, C6, Banco Inter, Rappi, Loggi, Memed, entre outras. Além dessas empresas, também integram a rede grandes executivos, como CTOs, CISOs e CIOs de destaque no mercado. O propósito da Rede é criar um ambiente que promova a conexão entre profissionais qualificados e empresas de relevância no setor digital. Sendo participante de projetos passados, fui reconhecida como Membro da Rede Líderes Digitais, o que me forneceu oportunidades de visibilidade e networking.</p>   
+                        <h1 className="mt-5 text-center text-xl font-bold">B3 - Brasil, Bolsa, Balcão</h1>
+                        <p className="text-center">Fevereiro de 2025</p>
+                        <p className="text-center mb-5">Função: Estágio em Infraestrutura</p>
+                        <p className="">Estou atuando como estagiária na área de Sustentação de Infraestrutura da B3, onde tenho a oportunidade de desenvolver minhas habilidades em tecnologia e contribuir para a manutenção e evolução da infraestrutura que garante a segurança e disponibilidade das operações. Fazer parte de uma das maiores instituições financeiras do país tem sido uma experiência incrível, repleta de aprendizado e crescimento..</p>   
                     </div>       
                 </div>
             </div>
